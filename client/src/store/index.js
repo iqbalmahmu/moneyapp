8a2157861819c9b03e16d8dc48672bde3ffb9cbd
rad <mdrad43@gmail.com>
2024-06-23 15:42:55 +0800

    added thunk middleware inside store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,17 @@
 // import { createStore } from "redux";
 
-import { legacy_createStore as createStore } from "redux";
+import {
+  applyMiddleware,
+  compose,
+  legacy_createStore as createStore,
+} from "redux";
 
 import rootReducer from "./reducer/rootReducer";
 
-const store = createStore(rootReducer);
+import thunk from "redux-thunk";
+
+const middleware = [thunk];
+
+const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
 
 export default store;
